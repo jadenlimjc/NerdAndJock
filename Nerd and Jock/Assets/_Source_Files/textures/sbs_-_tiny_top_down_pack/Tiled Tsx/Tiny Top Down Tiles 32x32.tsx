a3b96f30d9404c05d8d975f0cabad6563a9c47a5
Jaden Lim <jadenlimjc@gmail.com>
2024-05-21 22:29:45 +0800

    added some texture files

---
+++ Nerd and Jock/Assets/_Source_Files/textures/sbs_-_tiny_top_down_pack/Tiled Tsx/Tiny Top Down Tiles 32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tiny Top Down Tiles" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../Tiny Top Down 32x32.png" trans="008080" width="320" height="320"/>
+</tileset>
