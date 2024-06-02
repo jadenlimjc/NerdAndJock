dc1bf3b812b4d41d6bea8c96155b1c30115c24eb
Jaden Lim <jadenlimjc@gmail.com>
2024-06-02 20:43:16 +0800

    fixed multiple features
    1. created textbubble prefab
    2. created dialogue activation script to activate dialogue only upon detection
    3. added TMP files
    4. removed button text from homescreen

---
+++ Nerd and Jock/Assets/Scripts/dialogueActivator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueActivator : MonoBehaviour
+{
+    public GameObject dialogue;
+    public GameObject player;
+    public float activationRange = 5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate distance between player and object
+        float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+         Debug.Log("distance to player: " + distanceToPlayer);
+
+        //check if player in activation range
+        if (distanceToPlayer < activationRange) {
+            dialogue.SetActive(true);
+        }
+        else
+            {
+                // Deactivate the target object
+                dialogue.SetActive(false);
+            }
+    }
+}
