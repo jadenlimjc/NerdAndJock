681b6e8952d3ed1daf2fe050ff8fef71fa92f7e2
kvernnn <156299531+kvernnn@users.noreply.github.com>
2024-06-02 11:59:24 +0800

    tried to fix camera logic for death logic but not working

--- Nerd and Jock/Assets/Scripts/FollowPlayer.cs
+++ Nerd and Jock/Assets/Scripts/FollowPlayer.cs
@@ -6,8 +6,13 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player1;
     public GameObject player2;
+    public LayerMask groundLayer;
+    public Transform groundCheck1;
+    public Transform groundCheck2;
     // Start is called before the first frame update
     private Vector3 offset = new Vector3(0,0,-3);
+    private bool isP1Grounded = false;
+    private bool isP2Grounded = false;
 
 
     void Start()
@@ -18,8 +23,20 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = (player1.transform.position 
-                             + player2.transform.position) / 2 
-                             + offset;
+        float midX = (player1.transform.position.x 
+                    + player2.transform.position.x) / 2;
+                    
+        float midY = transform.position.y;
+        /* transform.position.x = (player1.transform.position.x 
+                             + player2.transform.position.x) / 2 
+                             + offset; */
+        isP1Grounded = Physics2D.OverlapCircle(groundCheck1.position, 0.1f, groundLayer);
+        isP2Grounded = Physics2D.OverlapCircle(groundCheck2.position, 0.1f, groundLayer);
+        if (isP1Grounded && isP2Grounded) {
+            midY = (player1.transform.position.y 
+                    + player2.transform.position.y) / 2; 
+        }
+
+        transform.position = new Vector3(midX, midY, offset.z);
     }
 }
